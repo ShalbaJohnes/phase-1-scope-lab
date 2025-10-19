9bbf01b4a7a27667fc640604ea49a6f066fe75c5
ShalbaJohnes <wawerudebbie@gmail.com>
2025-10-19 06:12:56 +0300

    made changes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+// Declare variables properly
+var customerName = 'bob';
+let bestCustomer; // let allows reassignment later
+let leastFavoriteCustomer = 'some initial value'; // changed from const to let
+
+// Function to capitalize customer name
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set the best customer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite the best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Function to change the least favorite customer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy';
+}
